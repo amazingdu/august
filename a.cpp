72e57f733a216e2610d061a29cf2f5f2e28033ef
568277997@qq.com <568277997@qq.com>
2018-09-09 06:08:18 +0800

    hello august

---
+++ a.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int test()
+{
+	cout<<"hello git"<<endl;
+	return 0;
+}
+
+int main()
+{
+	int n = 0;
+	n = test();
+	cout<<"n = "<< n << endl;
+
+	return 0;
+}
